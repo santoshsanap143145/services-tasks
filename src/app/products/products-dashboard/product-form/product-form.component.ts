eb2e5d77043a715e3da06b25271ade9e3c4776da
santosh sanap <santoshsanap143145@gmail.com>
2025-04-17 10:54:55 +0530

    corrected mistake in products module

--- src/app/products/products-dashboard/product-form/product-form.component.ts
+++ src/app/products/products-dashboard/product-form/product-form.component.ts
@@ -7,24 +7,24 @@ import { UuidService } from 'src/app/services/uuid.service';
 @Component({
   selector: 'app-product-form',
   templateUrl: './product-form.component.html',
-  styleUrls: ['./product-form.component.scss']
+  styleUrls: ['./product-form.component.scss'],
 })
 export class ProductFormComponent implements OnInit {
-  @ViewChild('productForm') productForm !: NgForm
-  constructor(private _uuidService: UuidService,
+  @ViewChild('productForm') productForm!: NgForm;
+  constructor(
+    private _uuidService: UuidService,
     private _productService: ProductService
-  ) { }
+  ) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  submitOnClick() {
+    if (this.productForm.valid) {
+      let newProd: Iproduct = this.productForm.value;
+      newProd.pid = this._uuidService.uuid();
+      newProd.pstatus = ProductStatus.InProgress;
+      this._productService.createNewProd(newProd);
+      this.productForm.resetForm();
+    }
   }
-
-  submitOnClick(){
-    let newProd : Iproduct =this.productForm.value;
-    newProd.pid = this._uuidService.uuid();
-    newProd.pstatus = ProductStatus.InProgress;
-    this._productService.createNewProd(newProd)
-    this.productForm.resetForm()
-  }
-
-
 }
